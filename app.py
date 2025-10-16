8fe8cc8e53d8370cbcd97dfdb931daf2a893857d
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-10-16 20:48:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from datetime import datetime
 from tinydb import TinyDB, Query
 
 # --- PAGE CONFIG ---
-st.set_page_config(page_title="Z", page_icon="♾️", layout="centered")
+st.set_page_config(page_title="", page_icon="♾️", layout="centered")
 
 # --- CONFIGURE GEMINI API ---
 try:
